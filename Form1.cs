dcb2e0b46cdbddb974e276e3fb2764727296d13e
SaraAkmal <48750726+SaraAkmal@users.noreply.github.com>
2020-04-03 23:57:22 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Analyzer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender,EventArgs e) {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+
+        }
+
+        private void button1_Click(object sender,EventArgs e) {
+            var str = richTextBox1.Text;
+            Scanner.Token c = new Scanner.Token();
+            c.getToken(str);
+            richTextBox2.Text = c.slicer(c.GetText());
+        }
+    }
+}
